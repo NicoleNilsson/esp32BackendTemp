99b2ae405fb512e56d1b79bd77175afff7f9977f
NicoleNilsson <nicole-nilsson@outlook.com>
2024-09-17 14:36:39 +0200

    Initial commit

---
+++ src/main/java/com/example/esp32backendtemp/Esp32BackendTempApplication.java
@@ -0,0 +1,13 @@
+package com.example.esp32backendtemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esp32BackendTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esp32BackendTempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esp32backendtemp/Esp32BackendTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esp32backendtemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esp32BackendTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
