73f44f7abc0064fdbae616c4627fe5a8ef13f00b
NicoleNilsson <nicole-nilsson@outlook.com>
2024-09-18 16:20:16 +0200

    Made Sensor name unique. In SensorController: changed method add to take PathVariable and updated method getByName to return Sensor In SensorRepo: updated findByName to return Sensor

--- src/main/java/com/example/esp32backendtemp/repositories/SensorRepo.java
+++ src/main/java/com/example/esp32backendtemp/repositories/SensorRepo.java
@@ -3,8 +3,6 @@ package com.example.esp32backendtemp.repositories;
 import com.example.esp32backendtemp.models.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SensorRepo extends JpaRepository<Sensor, Long> {
-    List<Sensor> findByName(String name);
+    Sensor findByName(String name);
 }
